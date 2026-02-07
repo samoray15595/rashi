ee8ecdfb78fca09304e755d2fadcf4ef8f355ecd
samoray15595 <samoray15595@gmail.com>
2026-02-07 03:52:03 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
+    'process.env': {
+      API_KEY: JSON.stringify(process.env.API_KEY)
+    }
   }
 });
